506b5dd9136ecda056a118322c977cf476cd8fa5
Ett05 <itsmeskittle123@gmail.com>
2024-09-28 17:53:06 -0400

    Main

--- src/main/java/org/translation/Main.java
+++ src/main/java/org/translation/Main.java
@@ -1,7 +1,6 @@
 package org.translation;
 
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -16,7 +15,6 @@ import java.util.Scanner;
  * ginky ponders
  */
 public class Main {
-
     /**
      * This is the main entry point of our Translation System!<br/>
      * A class implementing the Translator interface is created and passed into a call to runProgram.
@@ -41,18 +39,11 @@ public class Main {
      */
     public static void runProgram(Translator translator) {
         String quit = "quit";
-        // should I make this public static String = "quit" ??;
         while (true) {
             String country = promptForCountry(translator);
-            // TODO CheckStyle: The String "quit" appears 3 times in the file.
-            // TODO Checkstyle: String literal expressions should be on the left side of an equals comparison
             if (quit.equals(country)) {
                 break;
             }
-            // TODO Task: Once you switch promptForCountry so that it returns the country
-            //            name rather than the 3-letter country code, you will need to
-            //            convert it back to its 3-letter country code when calling promptForLanguage
-
             String countryCode = null;
 
             for (String code : translator.getCountries()) {
@@ -65,12 +56,9 @@ public class Main {
             if (quit.equals(language)) {
                 break;
             }
-            // TODO Task: Once you switch promptForLanguage so that it returns the language
-            //            name rather than the 2-letter language code, you will need to
-            //            convert it back to its 2-letter language code when calling translate.
-            //            Note: you should use the actual names in the message printed below though,
-            //            since the user will see the displayed message.
-            System.out.println(country + " in " + language + " is " + translator.translate(country, language));
+            LanguageCodeConverter languageConverter = new LanguageCodeConverter();
+            System.out.println(language + "This is language");
+            System.out.println(country + " in " + language + " is " + translator.translate(country, languageConverter.fromLanguage(language)));
             System.out.println("Press enter to continue or quit to exit.");
             Scanner s = new Scanner(System.in);
             String textTyped = s.nextLine();
@@ -84,12 +72,6 @@ public class Main {
     // Note: CheckStyle is configured so that we don't need javadoc for private methods
     private static String promptForCountry(Translator translator) {
         List<String> countries = translator.getCountries();
-        // TODO Task: replace the following println call, sort the countries alphabetically,
-        //            and print them out; one per line
-        //      hint: class Collections provides a static sort method
-        // TODO Task: convert the country codes to the actual country names before sorting
-        System.out.println(countries);
-
         for (int i = 0; i < countries.size(); i++) {
             countries.set(i, translator.translate(countries.get(i), "en"));
         }
@@ -108,14 +90,11 @@ public class Main {
 
     // Note: CheckStyle is configured so that we don't need javadoc for private methods
     private static String promptForLanguage(Translator translator, String country) {
-
-        // TODO Task: replace the line below so that we sort the languages alphabetically and print them out; one per line
-        // TODO Task: convert the language codes to the actual language names before sorting
         List<String> languages = translator.getCountryLanguages(country);
-        System.out.println(languages);
+        LanguageCodeConverter languageConverter = new LanguageCodeConverter();
 
         for (int i = 0; i < languages.size(); ++i) {
-            languages.set(i, translator.translate((String) languages.get(i), "en"));
+            languages.set(i, languageConverter.fromLanguageCode(languages.get(i)));
         }
         Collections.sort(languages);
         for (String language : languages) {
